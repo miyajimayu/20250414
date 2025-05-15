f4ca0f7661169d1c9e0c3edf7de550fd669bc38e
miyajimaisamuto <miyajimaisamuto@localhost>
2025-05-16 08:57:38 +0900

    java1.3.2

---
+++ HelloWorld_MiyajimaYuto/src/sample/SelfIntroduction.java
@@ -0,0 +1,27 @@
+package sample;
+
+public class SelfIntroduction {
+	// 氏名
+	String name = "山田花子";
+	//　年齢
+	int age = 23;
+	String hobby = "映画観賞";
+	
+	/*
+	 * 自己紹介文を出力する処理
+	 */
+	
+	public static void main(String[] args){
+		String name ="宮島勇斗";
+		int age = 23;
+		String hobby ="映画鑑賞";
+
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です");
+		System.out.println("趣味は" + hobby + "です。");
+	
+	
+	
+	}
+			
+}
